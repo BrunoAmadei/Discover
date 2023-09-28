76d67a8b905c9d76f7db4044695712998a31012a
BrunoAmadei <brunoamadei98@gmail.com>
2023-09-28 13:31:01 -0300

    script update

---
+++ theme.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+    const body = document.body
+
+    body.classList.toggle('light')
+    // if(body.classList.contains('light')){
+    //     body.classList.remove('light')
+    // }else{
+    //     body.classList.add('light')
+    // }
+}
