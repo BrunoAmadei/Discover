c57f4365e530e581e6536e749ac960bbd6ae2f02
BrunoAmadei <brunoamadei98@gmail.com>
2023-10-03 13:17:06 -0300

    update script

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+    const body = document.body
+    body.classList.toggle('light')
+
+    const img = document.querySelector('.profile img')
+    if (body.classList.contains('light')) {
+        img.setAttribute('src', './images/avatar-light.png')
+    }else{
+        img.setAttribute('src', './images/avatar.png')
+    }
+}
